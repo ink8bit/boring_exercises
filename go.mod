cc0bd73b3c8c69dbee5f1c275f8aa3f4cfe3c566
ink8bit <ink8bit@users.noreply.github.com>
2019-03-03 14:41:32 +0300

    Add cli package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ink8bit/go_hello
 
 go 1.12
+
+require github.com/urfave/cli v1.20.0
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
+// This is a simple CLI app. It has a few commands:
+// * ns - retrieve the name servers
+// * cname - lookup the CNAME for a given host
+// * mx - lookup the mail exchange records for a given host
+// * ip - lookup the IP addresses for a given host
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	fmt.Println("hello")
+	err := cli.NewApp().Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
