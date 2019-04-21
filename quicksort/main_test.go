0d67d12f82c0742fbbb5535b7e6c0f50b8f71294
ink8bit <ink8bit@users.noreply.github.com>
2019-04-21 14:47:05 +0300

    Add test for quicksort

---
+++ quicksort/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var testCases = []struct {
+	in  []int
+	out []int
+}{
+	{
+		in:  []int{3, 2, 1, 4, 5},
+		out: []int{1, 2, 3, 4, 5},
+	},
+	{
+		in:  []int{17, 20, 110, 5, 55},
+		out: []int{5, 17, 20, 55, 110},
+	},
+	{
+		in:  []int{5},
+		out: []int{5},
+	},
+}
+
+func TestQuicksort(t *testing.T) {
+	for _, tt := range testCases {
+		arr := tt.in
+		want := tt.out
+		got := quicksort(tt.in)
+
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("❌ arr: is %v, got: %v, but wanted: %v", arr, got, want)
+			}
+		})
+	}
+}
